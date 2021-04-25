d0a8213210f8f01f80e13829d871517842dc90d0
nargok <nargok@gmail.com>
2021-04-26 08:32:34 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by 後閑諒一 on 2021/04/26.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
