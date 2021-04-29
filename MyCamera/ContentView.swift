b2d9a4a9bd7906b424a029debc053c3fa340cd64
nargok <nargok@gmail.com>
2021-04-29 11:33:49 +0900

    show camera display

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -8,9 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var captureImage: UIImage? = nil
+    @State var isShowSheet = false
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack {
+            Spacer()
+            
+            if let unwrapCaptureImage = captureImage {
+                Image(uiImage: unwrapCaptureImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            
+            Spacer()
+            
+            Button(action: {
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    print("you can use camera!")
+                    isShowSheet = true
+                } else {
+                    print("you can not use camera...orz")
+                }
+            }) {
+                Text("カメラを起動する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+            }
             .padding()
+            .sheet(isPresented: $isShowSheet) {
+                ImagePickerView(
+                    isShowSheet: $isShowSheet,
+                    captureImage: $captureImage)
+            }
+        }
     }
 }
 
---
+++ MyCamera/ImagePickerView.swift
@@ -0,0 +1,67 @@
+//
+//  ImagePickerView.swift
+//  MyCamera
+//
+//  Created by 後閑諒一 on 2021/04/26.
+//
+
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    
+    
+    @Binding var isShowSheet: Bool
+    @Binding var captureImage: UIImage?
+    
+    class Coordinator: NSObject,
+                       UINavigationControllerDelegate,
+                       // UIImagerPickerControllerの操作をdelegateで検知する
+                       UIImagePickerControllerDelegate {
+        let parent: ImagePickerView
+        init (_ parent: ImagePickerView) {
+            self.parent = parent
+        }
+        
+        // 撮影が終わったときに呼ばれるdelegateメソッド
+        func imagePickerController(
+            _ picker: UIImagePickerController,
+            didFinishPickingMediaWithInfo info:
+                [UIImagePickerController.InfoKey : Any]) {
+            
+            // 撮影した写真をcaptureImageに保存
+            if let originalImage =
+                info[UIImagePickerController.InfoKey.originalImage]
+                as? UIImage {
+                parent.captureImage = originalImage
+            }
+            
+            parent.isShowSheet = false
+        }
+        
+        // キャンセルボタンを選択したときに呼ばれるdelegateメソッド
+        func imagePickerControllerDidCancel(
+            _ picker: UIImagePickerController) {
+            parent.isShowSheet = false
+        }
+        
+    }
+    
+    // Coordinatorを生成。SwiftUIによって自動的に呼び出し
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    // Viewを生成する時に実行
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePickerView>) -> UIImagePickerController {
+        let myImagePickerController = UIImagePickerController()
+        myImagePickerController.sourceType = .camera
+        myImagePickerController.delegate = context.coordinator
+        return myImagePickerController
+    }
+    
+    // Viewが更新された時に実行
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePickerView>) {
+
+    }
+    
+}
